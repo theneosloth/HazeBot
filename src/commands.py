fdbb7a18e7c44a2c7fbe3333f1a3506d5f2045a9
superstepa <superstepag@gmail.com>
2016-06-05 18:41:00 -0700

    Added the help command

--- src/commands.py
+++ src/commands.py
@@ -21,7 +21,7 @@ user: discord.Client
 async def ping_command(parent, args, user):
     await parent.say("Hello, {0}!".format(user.name))
 
-@command("!help")
+@command("!commands")
 async def command_help(parent, args, user):
     message = ""
     for command in parent.yield_commands():
@@ -55,3 +55,34 @@ async def roll_command(parent, args, user):
             range = 6
     roll = randrange(1, range)
     await parent.say("Rolled {0} on a {1} sided die.".format(roll, range))
+
+@command("!help")
+async def command_help(parent, args, user):
+    """Loads help for a game from a help.json file"""
+
+    from json import loads
+    import os
+
+    ERROR_MSG = "Invalid format, expected format is: \"!help UT99/UT2004/JA \"."
+    ERROR_MSG += "\nFor connection info use !help UT99/UT2004/JA connect"
+    if (len(args) < 1):
+        await parent.say(ERROR_MSG)
+        return
+
+    FILE_NAME = "../data/help.json"
+    path = os.path.join(os.path.dirname(__file__), FILE_NAME)
+    default_request = "general"
+
+    try:
+        game = args[0].lower()
+        request = args[1].lower()
+    except IndexError:
+        request = default_request
+
+    with open(path, 'r') as f:
+        data = loads(f.read())
+        try:
+            msg = data[game][request]
+        except KeyError:
+            msg = ERROR_MSG
+        await parent.say(msg)
